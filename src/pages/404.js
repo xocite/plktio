be008b638eae80464ead83a15ce3d93077f82dd5
Antony Jepson <a@plkt.io>
2019-05-29 14:33:04 +0000

    Update 404 page to point to git repository.

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Page doesn't exist.  Need it?  Create it.  <a href="https://github.com/xocite/plktio">GitHub repository</a></p>
   </Layout>
 )
 
