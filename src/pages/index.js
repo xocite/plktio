0e320b2a8e7bb4a8a91d4718d7263c016a2b7c33
Antony Jepson <a@plkt.io>
2019-05-29 14:33:44 +0000

    Replace boilerplate text on index page with reference to coming soon.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>plkt.io</h1>
+    <p>Work in progress site.</p>
+    <p>The future home for antonyjepson.co.uk</p>
   </Layout>
 )
 
