70741d7c6bad55b8d20420894b3c5aa1f555eda6
Antony Jepson <a@plkt.io>
2019-05-31 16:24:22 +0100

    Add support for Markdown files.

---
+++ src/posts/welcome.md
@@ -0,0 +1,7 @@
+---
+path: "/my-first-post"
+date: "2019-05-31"
+title: "My first post"
+---
+
+Please include this post.
---
+++ src/templates/blogTemplate.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export default function Template({
+  data, // injected by GraphQL query 
+}) {
+  const { markdownRemark } = data 
+  const { frontmatter, html } = markdownRemark
+  return (
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  )
+}
+
+// https://graphql.org/graphql-js/type/#graphqlinputobjecttype
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "DD MMMM, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
