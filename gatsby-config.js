c609edbcd33fde919d75cf541203b4c98f2e1110
Antony Jepson <a@plkt.io>
2019-06-03 01:00:13 +0100

    Clarify the site metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `plkt.io`,
+    description: `The best way to see and get involved in what I'm doing.`,
+    author: `@plktio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
