da7cce04751a17724927e76c49a9a4568f353124
Antony Jepson <a@plkt.io>
2019-06-06 21:52:16 +0100

    Clean header by removing purple bar.

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,16 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header>
+    <div style={{margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem`}}>
+      <h1>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
-          {siteTitle}
+          plkt.io
         </Link>
       </h1>
     </div>
@@ -36,7 +22,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `plkt.io`,
 }
 
 export default Header
