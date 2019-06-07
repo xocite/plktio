5f5beaa54411a18f3053256b3fa94f052fe75b39
Antony Jepson <a@plkt.io>
2019-06-07 01:08:46 +0100

    Clean up index copy.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h3>Hi <span role="img" aria-label="Waving hand">👋</span>  I’m Antony, a technical product manager.  I can help you build quality products.</h3>
+      <h3>Hi <span role="img" aria-label="Waving hand">👋</span>  I’m Antony, a technical product manager.  I help you build quality products.</h3>
       <p>I've written {data.allMarkdownRemark.totalCount} posts in 2019.</p>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
