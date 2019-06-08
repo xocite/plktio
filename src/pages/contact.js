253f651f9476e421e916404e1cc2b0148380191f
Antony Jepson <a@plkt.io>
2019-06-08 21:37:51 +0100

    Add contact page.

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Contact Antony" />
+    <h1>Contact</h1>
+    <p>For inquiries about existing or future suggested writing topics: email "a" at this website.</p>
+    <p>For professional inquiries: LinkedIn <i><a href="https://www.linkedin.com/in/antony-jepson">Antony Jepson</a></i>.</p>
+    <p>For everything else, Twitter: <a href="https://twitter.com/plktio">@plktio</a>.</p>
+  </Layout>
+)
+
+export default Contact
