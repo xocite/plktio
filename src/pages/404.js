54bc349b79b53a131c2d2f5c0265bfb353895d6a
Antony Jepson <a@plkt.io>
2019-06-08 23:16:17 +0100

    Remove caps from 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>Page doesn't exist.  Need it?  Create it.  <a href="https://github.com/xocite/plktio">GitHub repository</a></p>
+    <h1>Not found</h1>
+    <p>Page doesn't exist.  Need it?  Create it.  <a href="https://github.com/xocite/plktio">GitHub repository.</a></p>
   </Layout>
 )
 
