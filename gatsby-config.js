0f93e1eae3d4850d57c4bd5fdcca6ee61aca1501
Antony Jepson <a@plkt.io>
2019-06-08 21:10:22 +0100

    Remove offline PWA support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `The best way to see and get involved in what I'm doing.`,
     author: `@plktio`,
   },
+  pathPrefix: `/blog`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -23,20 +24,5 @@ module.exports = {
       }
     },
     `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
