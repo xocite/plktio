e4a1e199a52c360c4d2b294764e63ae22a2d1548
Antony Jepson <a@plkt.io>
2019-06-09 00:34:12 +0100

    Add basic about page.

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="About Antony" />
+    <h1>About</h1>
+    <p>Learn more about <a href="https://antonyjepson.co.uk/about">me</a>.</p>
+  </Layout>
+)
+
+export default About
