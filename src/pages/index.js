598859dc5dabbb15bc09aa3c273855fbb79a8467
Antony Jepson <a@plkt.io>
2019-06-12 03:17:16 +0100

    Add link to my old home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,15 @@ export default ({ data }) => {
     <Layout>
       <SEO title="Home" />
       <h3>Hi <span role="img" aria-label="Waving hand">👋</span>  I’m Antony, a technical product manager.  I help you build quality products.</h3>
-      <p>I've written {data.allMarkdownRemark.totalCount} posts in 2019.</p>
+      <p>I've written {data.allMarkdownRemark.totalCount} posts since June 2019.</p>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <Link to={node.frontmatter.path}><strong>{node.frontmatter.title}</strong></Link> on {node.frontmatter.date}
         </div>
       )
       )}
+      <br />
+      80 older posts also available on my <a href="https://antonyjepson.co.uk">former home</a>.
     </Layout>
   )
 }
