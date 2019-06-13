ab17adb6056b29a9dd37e2a3361ea5d0eddf1f5c
Antony Jepson <a@plkt.io>
2019-06-13 09:41:18 +0100

    Remove unused import.

--- src/components/git_details.js
+++ src/components/git_details.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 const GitDetails = () => {
   const initialState = JSON.parse('{"htmlUrl": "", "hash": ""}')
