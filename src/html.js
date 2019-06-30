ffd8f62d34a5ffd6bd7d7a7c8379ece44302bb91
Antony Jepson <a@plkt.io>
2019-06-30 20:12:46 +0100

    Enable analytics on develop branch.

--- src/html.js
+++ src/html.js
@@ -1,6 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
-import favicon from './images/favicon.ico';
+import favicon from './images/favicon.ico'
+import Amplify, { Analytics } from 'aws-amplify'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
 
 export default function HTML(props) {
   return (
