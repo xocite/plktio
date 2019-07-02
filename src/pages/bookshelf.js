1af5faa2df821dd08acff0507a44512f5d5bb009
Antony Jepson <a@plkt.io>
2019-07-03 00:47:04 +0100

    Add interactivity to bookshelf to change author, comments, etc.

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -1,43 +1,85 @@
 import React, { useState } from 'react'
 
 import { Link, StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery } from 'gatsby'
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-export default () => (
-  <StaticQuery
-  query={graphql`
-    query BlankQuery {
-      allBooksYaml {
-        edges {
-          node {
-            title
-            author
-            subtitle
-            amazon
-            goodreads
+const Bookshelf = () => {
+  const [ isDisplayingLinks, displayLinks ] = useState(false)
+  const [ isDisplayingAuthor, displayAuthor ] = useState(true)
+  const [ isDisplayingFullTitle, displayFullTitle ] = useState(false)
+  const [ isDisplayingComment, displayComment ] = useState(true)
+
+  const books = useStaticQuery(
+    graphql`
+      query {
+        allBooksYaml {
+          edges {
+            node {
+              title
+              author
+              subtitle
+              amazon
+              goodreads
+              comment
+            }
           }
         }
       }
-    }
-  `}
-  render={data => (
+    `
+  )
+
+  return (
     <Layout>
+      <SEO title="Bookshelf" />
       <p>
-        Here are the books I've read recently.  People often ask me for book recommendations so I decided to compile a list of impactful books across a wide range of genres including fiction and non-fiction.  I'll be adding to this page over time.
+        Here is a list of the books I've read recently.  It serves as a good example of what interests me.  I'll be adding to this page over time.  <Link to="/contact">Let me know</Link> if you also enjoyed these books or have others to recommend. 😀.
       </p>
+      <button onClick={() => displayFullTitle(!isDisplayingFullTitle)}>Toggle full title</button>
+      <button onClick={() => displayAuthor(!isDisplayingAuthor)}>Toggle author name</button>
+      <button onClick={() => displayLinks(!isDisplayingLinks)}>Toggle Amazon/Goodreads links</button>
+      <button onClick={() => displayComment(!isDisplayingComment)}>Toggle comments</button>
+      <p></p>
       <ul>
-        {data.allBooksYaml.edges.map( ({ node }) => (
-          <li key={node.id}>{node.title}
-          &nbsp;
-          <small><a href={node.amazon}>(Amazon)</a></small>
-          &nbsp;
-          <small><a href={node.goodreads}>(Goodreads)</a></small>
+        {books.allBooksYaml.edges.map( ({ node }) => (
+          <li key={node.id}><strong>{node.title}</strong>
+          {
+            isDisplayingFullTitle ?
+              <>
+                : <em>{node.subtitle}</em>
+              </>
+            : null
+          }
+          {
+            isDisplayingAuthor ?
+              <>
+               &nbsp;by {node.author}
+              </>
+            : null
+          }
+          {
+            isDisplayingLinks ? 
+              <>
+                <small>&nbsp;<a href={node.amazon}>(Amazon)</a></small>
+                <small>&nbsp;<a href={node.goodreads}>(Goodreads)</a></small>
+              </>
+            : null
+          }
+          {
+            isDisplayingComment && node.comment ? 
+              <>
+                <blockquote>&nbsp;{node.comment}</blockquote>
+              </>
+            : null
+          }
           </li>
         )
         )}
       </ul>
-    </Layout>
+    </Layout>    
   )
-  }
-  />
-)
+
+}
+
+export default Bookshelf
