49f1c9a03e29e53627eb342abc88543722b3e41f
Antony Jepson <a@plkt.io>
2019-07-13 00:35:52 +0100

    Change "since June 2019"

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default ({ data }) => {
     <Layout>
       <SEO title="Home" />
       <h3>Hi <span role="img" aria-label="Waving hand">👋</span>  I’m Antony, a technical product manager.  I help you build quality products.</h3>
-      <p>I've written {data.allMarkdownRemark.totalCount} posts since June 2019.</p>
+      <p>I've written {data.allMarkdownRemark.totalCount} posts since July 2019.</p>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <Link to={node.frontmatter.path}><strong>{node.frontmatter.title}</strong></Link> on {node.frontmatter.date}
