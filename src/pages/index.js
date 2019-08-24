b0ec99e680432b44e7a91a59864978dfb479b714
Xocite <git@xocite>
2019-08-24 10:37:54 +0100

    Remove unused import.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import containerStyles from "../components/index.module.css"
 
 import { graphql, Link } from "gatsby"
 
