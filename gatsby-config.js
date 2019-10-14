164b5f69a3d32cd3fc6c417331e1f3cd137a7d39
Xocite <git@xocite>
2019-10-14 21:31:06 +0100

    Remove further references to Amplify and Google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-144235537-1`
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
